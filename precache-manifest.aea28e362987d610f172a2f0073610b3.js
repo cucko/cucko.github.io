eb556e0b03110156d89f2d8c85235e915a509baf
cucko <cucko@webthink.com>
2019-09-25 17:43:00 +0200

    Updates

---
+++ precache-manifest.aea28e362987d610f172a2f0073610b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4a83b0a4847c9090f162fa21929197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e192f79e2f6d09df230",
+    "url": "/static/css/main.14547379.chunk.css"
+  },
+  {
+    "revision": "c5047ea2ed20583bf658",
+    "url": "/static/js/2.0b5eef82.chunk.js"
+  },
+  {
+    "revision": "5e192f79e2f6d09df230",
+    "url": "/static/js/main.5de2ecfc.chunk.js"
+  },
+  {
+    "revision": "a7b9f716fd298d25e928",
+    "url": "/static/js/runtime-main.b5aad2f7.js"
+  }
+]);
